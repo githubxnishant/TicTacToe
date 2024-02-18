2310216ba19b7ec685aad9a9938474fcd7f730c5
Nishant <chauhannishant021>
2024-02-18 09:41:30 +0530

    update

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // JS code here
+
+ function redirectToPlay() {
+    window.location.href = "play.html";
+};
+
+function redirectToHome() {
+    window.location.href = "index.html";
+};
+
+function redirectToRules() {
+    window.location.href = "rules.html";
+};
+
+function redirectToDisclaimer() {
+    window.location.href = " disclaimer.html";
+};
